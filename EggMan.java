ff36cb4e22010bd8edee9b50f872525e4bb4bd2b
nayaadla <168391182+nayaadla@users.noreply.github.com>
2024-11-22 22:55:34 -0500

    Add files via upload

---
+++ EggMan.java
@@ -0,0 +1,95 @@
+
+public class EggMan {
+    public static void main(String[] args) {
+        printHeader();
+        printHexagon();
+        printLine();
+        printStopSign();
+        printHexagon();
+        printLine();
+        printDiamond();
+        printLine();
+        printSquare();
+        printLine();
+        printUpperHexagon();
+        printMiddleLine();
+        printLowerHexagon();
+        printFooter();
+    }
+
+    public static void printHeader() {
+        System.out.println("_____");
+        System.out.println("Hello World!");
+        System.out.println("This is my first Java Program!!");
+        System.out.println();
+    }
+
+    public static void printHexagon() {
+        System.out.println("  ______");
+        System.out.println(" /      \");
+        System.out.println("/        \");
+        System.out.println("\        /");
+        System.out.println(" \______/");
+        System.out.println();
+    }
+
+    public static void printLine() {
+        System.out.println("+--------+");
+        System.out.println();
+    }
+
+    public static void printStopSign() {
+        System.out.println("  ______");
+        System.out.println(" /      \");
+        System.out.println("/   STOP \");
+        System.out.println("\        /");
+        System.out.println(" \______/");
+        System.out.println();
+    }
+
+    public static void printDiamond() {
+        System.out.println("   /\");
+        System.out.println("  /  \");
+        System.out.println(" /    \");
+        System.out.println(" \    /");
+        System.out.println("  \  /");
+        System.out.println("   \/");
+        System.out.println();
+    }
+
+    public static void printSquare() {
+        System.out.println(" ______");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("|______|");
+        System.out.println();
+    }
+
+    public static void printUpperHexagon() {
+        System.out.println("  ______");
+        System.out.println(" /      \");
+        System.out.println("/        \");
+        System.out.println("\        /");
+        System.out.println(" \______/");
+        System.out.println();
+    }
+
+    public static void printMiddleLine() {
+        System.out.println("+--------+");
+        System.out.println();
+    }
+
+    public static void printLowerHexagon() {
+        System.out.println("  ______");
+        System.out.println(" /      \");
+        System.out.println("/        \");
+        System.out.println("\        /");
+        System.out.println(" \______/");
+        System.out.println();
+    }
+
+    public static void printFooter() {
+        System.out.println("Go Hatters!!");
+        System.out.println("Program by: Naya Adla");
+    }
+}
